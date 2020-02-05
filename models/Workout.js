6fc4ff4c66309aed44024124d3e8382a97030501
Becca Burke <reburke286@gmail.com>
2020-02-04 20:16:49 -0800

    updated get requests for html pages

--- models/Workout.js
+++ models/Workout.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  title: String,
-  body: String
+  type: String,
+  workout: String,
+  userCreated: {
+    type: Date,
+    default: Date.now
+  },
+  userUpdated: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -1,42 +1,69 @@
 const express = require("express");
 // const logger = require("morgan");
 const mongoose = require("mongoose");
-const materialize = require("materialize-css@next");
 
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
 
 const app = express();
+const path = require("path");
+const router = express.Router();
+
+// set static directories
+app.use(express.static(path.join(__dirname, "public")));
 
 // app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workoutdb", {
   useNewUrlParser: true
 });
 
-// db.User.create({ name: "Ernest Hemingway" })
-//   .then(dbUser => {
-//     console.log(dbUser);
-//   })
-//   .catch(({ message }) => {
-//     console.log(message);
-//   });
+db.Workout.create({})
+  .then(dbUser => {
+    console.log(dbUser);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
 
-// app.get("/notes", (req, res) => {
-//   db.Note.find({})
-//     .then(dbNote => {
-//       res.json(dbNote);
+app.get("/update", function(req, res) {
+  res.sendFile(path.join(__dirname + "/public/update.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname + "/public/add.html"));
+});
+
+// router.get("/update", (req, res) => {
+//   res.sendFile("update.html");
+//   db.Workout.find({})
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
 //     })
 //     .catch(err => {
 //       res.json(err);
 //     });
 // });
+
+app.post("/api/workouts", ({ body }, res) => {
+  const workout = new Workout(body);
+
+  workout
+    .create(workout)
+    .then(dbWorkout => {
+      // res.json(dbWorkout);
+      console.log(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 // app.get("/user", (req, res) => {
 //   db.User.find({})
