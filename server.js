6d68b36f494d005dab606b6a972ce1632febdac7
Becca Burke <reburke286@gmail.com>
2020-02-05 18:36:28 -0800

    getting update.handlebars to work, not done yet

--- server.js
+++ server.js
@@ -28,34 +28,45 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workoutdb", {
   useNewUrlParser: true
 });
 
-// creating the mongoose database
-db.Workout.create({})
-  .then(dbWorkout => {
-    console.log(dbWorkout);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
-
 // get requests
 app.get("/", function(req, res) {
   res.render("index");
 });
+
 app.get("/update", function(req, res) {
-  // db.Workout.find()
-  //   .then(dbWorkout => {
-  res.render("partials/update");
-  // })
-  // .catch(err => {
-  //   res.json(err);
-  // });
+  const workoutArr = [];
+  db.Workout.find({})
+    .then(dbWorkout => {
+      dbWorkout.forEach(function(doc, err) {
+        workoutArr.push(doc);
+      });
+    })
+    .then(() => {
+      res.render("partials/update", { workouts: workoutArr });
+    });
 });
+
+// router.get('/get-data', function(req, res, next) {
+//   var resultArray = [];
+//   mongo.connect(url, function(err, db) {
+//     assert.equal(null, err);
+//     var cursor = db.collection('user-data').find();
+//     cursor.forEach(function(doc, err) {
+//       assert.equal(null, err);
+//       resultArray.push(doc);
+//     }, function() {
+//       db.close();
+//       res.render('index', {items: resultArray});
+//     });
+//   });
+// });
 
 app.get("/add", function(req, res) {
   res.render("partials/add");
 });
 
 // post requests
+
 app.post("/add", ({ body }, res) => {
   db.Workout.create(body)
     .then(dbWorkout => {
@@ -66,12 +77,6 @@ app.post("/add", ({ body }, res) => {
       res.json(err);
     });
 });
-
-// app.post("/update", ({ body }, res) => {
-// db.Workout.create(body).then({_id}) =>
-// db.Workout.findOneAndUpdate({}, {$set: {title: }, {workout: }})
-
-// });
 
 app.get("/api/workouts", (req, res) => {
   db.Workout.find({})
